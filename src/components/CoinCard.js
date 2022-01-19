228be451e4b9795f0df2ad582a44e94faf6310db
Gustavo <gar.santeliz@gmail.com>
2022-01-18 19:13:03 -0500

    setting styles

---
+++ src/components/CoinCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native'
+import {images} from '../Utils/CoinIcons'
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        marginBottom: 20,
+        borderBottomColor: '#e5e5e5',
+        borderBottomWidth: 3,
+        padding: 20
+    },
+    upperRow: {
+        display: 'flex',
+        
+    }
+})
