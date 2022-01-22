418212efd081f1a057192ec2cc02d97d20111fc6
Gustavo <gar.santeliz@gmail.com>
2022-01-22 13:35:01 -0500

    re structuring code

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux'
+
+import Store from './src/Component';
+import { Header, CryptoContainer } from './src/Component'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's play with an app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider>
+      <View>
+        <Header/>
+        <CryptoContainer/>
+      </View>
+    </Provider>);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
