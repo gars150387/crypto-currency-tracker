84197eb93a5afe1aa9919697d5f27c44b1c78960
Gustavo <gar.santeliz@gmail.com>
2022-01-24 19:34:24 -0500

    add homepage in package.json

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
-import {apiBaseURL} from './../Utils/Constants'
 import { apiBaseURL } from './../Utils/Constants'
-import {FETCHING_COIN_DATA,FETCHING_COIN_DATA_SUCCESS, FETCHING_COIN_DATA_FAIL} from './../Utils/ActionTypes'
+import { apiBaseURL } from './../Utils/Constants'
+import { FETCHING_COIN_DATA, FETCHING_COIN_DATA_SUCCESS, FETCHING_COIN_DATA_FAIL } from './../Utils/ActionTypes'
 
-export const FetchCoinData =()=> {
-    return async dispatch =>{
-        dispatch({type: FETCHING_COIN_DATA})
+export const FetchCoinData = () => {
+    return dispatch => {
+        dispatch({ type: FETCHING_COIN_DATA })
+        console.log('fetching coin data', FETCHING_COIN_DATA)
 
-        try {
-            const res = await axios.get(`${apiBaseURL}/v1/cryptocurrency/listing/lastest?CMC_PRO_API_KEY=` + apiKey)
-            return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data })
-            console.log('dispatch',res.data)
-        } catch (error) {
-            return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: error })
-        }
+        const res = axios.get(`${apiBaseURL}/v1/cryptocurrency/listing/lastest?CMC_PRO_API_KEY=${apiKey}`)
+        return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data },
+            console.log('response', res.data))
+        
+            .catch(error)
+        return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: error },
+            console.log('error', error))
+        
     }
 }
